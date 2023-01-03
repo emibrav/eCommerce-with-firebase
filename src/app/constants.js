b7bf4d85ad58fc477371737464e6c0040689ee39
Luciano Bravo <bravoemiliano74@gmail.com>
2023-01-03 19:31:52 -0300

    Adding a new offcanvas cart

---
+++ src/app/constants.js
@@ -0,0 +1,12 @@
+export const INFO = {
+    avatar: "https://via.placeholder.com/150",
+    title: "Norma Accesorios",
+    description: "Cada accesorio es único e irrepetible. No se hacen 2 iguales",
+    phone: `3512297944`,
+    social: {
+      instagram: "instagram.com/",
+      facebook: "facebook.com/",
+      twitter: "twitter.com/"
+    },
+    sheet: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRUt25j0JAbKfP9-4nnemXKtOkrsbI7Ri1ssD_JAY17WpvKqAzHfQHn5Qq4M7S6a73lVU8U3Wm6f0wP/pub?output=csv"
+  }
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -2,17 +2,26 @@ import axios from "axios"
 import { useEffect, useState } from "react";
 import Papa from "papaparse"
 import Card from "../components/Card";
+import { INFO } from "../app/constants";
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import { Link } from "react-router-dom";
 
 const Home = () => {
   
   const [ APIdata, setAPIdata ] = useState([])
   const [ cart, setCart ] = useState([])
   const [ loading, setLoading ] = useState(false)
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   
   useEffect(() => {
     setLoading(true)
 
-    axios.get("https://docs.google.com/spreadsheets/d/e/2PACX-1vRUt25j0JAbKfP9-4nnemXKtOkrsbI7Ri1ssD_JAY17WpvKqAzHfQHn5Qq4M7S6a73lVU8U3Wm6f0wP/pub?output=csv",
+    axios.get(INFO.sheet,
     {
       responseType: "blob"
     }
@@ -36,33 +45,61 @@ const Home = () => {
   }, [])
   
   const handleAddToCart = (item) => {
-    setCart((cart) => cart.concat(item))
-    
+    setCart((cart) => cart.concat(item));
   }
 
   const text = cart.reduce((message, item) => message.concat(`\n• ${item.name} - $${item.price}`), ``).concat(`\n\nTotal: $${cart.reduce((total, item) => parseInt(total) + parseInt(item.price), 0) }`)
 
-  //Total: ${cart.reduce((total, item) => total + item.price, 0) }`)
+  let total = cart.reduce((total, item) => parseInt(total) + parseInt(item.price), 0)
   
   return (
     <>
       <div className="row text-center container mx-auto">
         {
-          loading ? <h4>Cargando productos...</h4> : <h3 className="my-3">Productos 🤩</h3>
+          loading ? <h4>Cargando productos...</h4> : <h3 className="my-3">{INFO.title} 🤩</h3>
         }
         {
           APIdata.map(item => (
             <div key={item.id} className="col-12 mb-3 col-md-6 col-lg-4 col-xl-3 d-flex justify-content-center">
               <Card product={item} handleAddToCart={() => handleAddToCart(item)} />
+              {
+                cart.length ? 
+                <div>
+                  <div className="d-flex w-100">
+                    <Button className="fixed-bottom m-auto w-75" variant="success" onClick={handleShow}>
+                      Ver Carrito ({cart.length} productos) - ${total}
+                    </Button>
+                  </div>
+
+                  <Offcanvas show={show} onHide={handleClose} className="">
+                    <Offcanvas.Header closeButton>
+                      <Offcanvas.Title>Carrito 🛒</Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                      {
+                        cart.map(item => (
+                          <div>
+                            <div key={item.id}>{item.name} - ${item.price}
+                              <button className="btn btn-primary btn-sm mx-1 my-1">+</button>
+
+                              <button className="btn btn-danger btn-sm">-</button>
+                            </div>
+                          </div>
+                        ))
+                      }
+                    </Offcanvas.Body>
+                      <a className="btn btn-success" href={`https://wa.me/5493512297944?text=${encodeURIComponent(text)}`}>Finalizar compra en Whatsapp</a>
+                  </Offcanvas>
+                </div>
+                :
+                null
+              }
             </div>
           ))
         }
-        {
-          cart.length ? 
-            <a href={`https://wa.me/5493512297944?text=Hola! Te paso mi lista de compra:${encodeURIComponent(text)}`} className="btn btn-success d-grid gap-2  sticky-bottom" onClick={() => console.log(cart)}>Finalizar compra en Whatsapp - ({cart.length} productos) - ${cart.reduce((total, item) => parseInt(total) + parseInt(item.price), 0)}</a>
-            : null
-        }
-      </div>  
+        
+      </div>
+      
     </>
   )
 }
